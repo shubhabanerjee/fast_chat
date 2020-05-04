260e86ec4b4e82d18f3f3a50b3ec90bd41424496
shubhabanerjee <shubhabanerjeewin8@gmail.com>
2020-05-04 20:35:25 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,34 @@
+
+import 'package:fastchat/resourses/firebase_repository.dart';
+import 'package:fastchat/screens/home_screen.dart';
+import 'package:fastchat/screens/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-void main()=> runApp(MaterialApp(home: MyApp()));
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  FirebaseRepository _repository = FirebaseRepository();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-          body: Center(
-            child: Container(
-              child: Text('data'),
-          
-        ),
+    return MaterialApp(
+      title: "Skype Clone",
+      debugShowCheckedModeBanner: false,
+      home: FutureBuilder(
+        future: _repository.getCurrentUser(),
+        builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+          if (snapshot.hasData) {
+            return HomeScreen();
+          } else {
+            return LoginScreen();
+          }
+        },
       ),
     );
   }
